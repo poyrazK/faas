cf4f0caaea53f0919f0cfa78a45abe95423d5538
Poyraz Küçükarslan <83272398+PoyrazK@users.noreply.github.com>
2026-09-08 12:20:44 +0300

    fix: format metrics wiring with CI Go version

--- pkg/wire/metrics.go
+++ pkg/wire/metrics.go
@@ -4609,7 +4609,7 @@ func NewOpsMetrics(prefix string) *OpsMetrics {
 		auditLogWriteTotal:                                    auditLogWriteTotal,
 		auditLogWriteFailuresTotal:                            auditLogWriteFailuresTotal,
 		operatorActionTraceCompletenessRatio:                  operatorActionTraceCompletenessRatio,
-		operatorActionTraceCompletenessFirstTickCompleted:     operatorActionTraceCompletenessFirstTickCompleted,
+		operatorActionTraceCompletenessFirstTickCompleted:   operatorActionTraceCompletenessFirstTickCompleted,
 		operatorActionTraceCompletenessLastSuccessTimestamp: operatorActionTraceCompletenessLastSuccessTimestamp,
 		uploadSessionCreatedTotal:                           uploadSessionCreatedTotal,
 		uploadSessionCommittedTotal:                         uploadSessionCommittedTotal,
